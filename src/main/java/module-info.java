676e2fb4ff49188f6ec1e5dbf9ffe2cf89333e5b
qkonstantin <kuznetsov.ks@edu.spbstu.ru>
2020-10-17 19:46:03 +0300

    TicTacToe

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens org.example to javafx.fxml;
     exports org.example;
